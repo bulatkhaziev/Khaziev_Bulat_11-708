32b3062cb0785fa5e3d4f23ef03c3cac2200fa10
khazievbulat <taras2631@gmail.com>
2017-09-12 17:41:18 +0300

    Обновлен алгоритм
    1. Обновлен алгоритм нахождения "счастливого числа": убрано
    использование строк и массивов.

--- 2017/FALL2017/SEM/PS1/Ex1.cs
+++ 2017/FALL2017/SEM/PS1/Ex1.cs
@@ -1,44 +1,28 @@
+/*
+ * Автор программы - Хазиев Булат 11-708.
+ * 
+ * Что делает программа:
+ *  - Программа считываем 6-ти значное число и проверяет, является ли оно "счастливым". Счастливое число - это когда сумма первых 3-х цифр равна сумме последних 3-х цифр.
+ */
+
 using System;
 
 namespace ConsoleApp
 {
-    class SwapNumbersProgram
+    class IsTicketHappy
     {
         public static void Main(string[] args)
         {
-            int[] ticketNumbers = { 123321, 122322, 453512 }; // Номера билетов для анализа.
-
-            for(int i = 0; i < ticketNumbers.Length; i++)
-            {
-                IsThisTicketHappy(ticketNumbers[i]);
-            }
-            Console.ReadKey();
-        }
-
-        static void IsThisTicketHappy(int ticketNumber)
-        {
-            string strTicketNumber = ticketNumber.ToString();
-            int ticketNumberLength = strTicketNumber.Length,
-                firstTicketPartSum = 0,
-                secondTicketPartSum = 0;
-
-            // Деление номера билета на две части.
-            string firstTicketPart = strTicketNumber.Substring(0, ticketNumberLength / 2), 
-                   secondTicketPart = strTicketNumber.Substring(ticketNumberLength / 2, ticketNumberLength / 2);
-
-            // Сложение цифр первой части билета.
-            for(int i = 0; i < firstTicketPart.Length; i++)
-                firstTicketPartSum += firstTicketPart[i];
-
-            // Сложение цифр второй части билета.
-            for (int i = 0; i < secondTicketPart.Length; i++)
-                secondTicketPartSum += secondTicketPart[i];
-
-            // Вывод результата анализа.
-            if (firstTicketPartSum == secondTicketPartSum)
-                Console.WriteLine("Наши поздравления! Билет №{0} является счастливым!", ticketNumber);
+            int ticketNumber = Convert.ToInt32(Console.ReadLine());
+           
+            int firstTicketPart = ticketNumber / 100000 + ticketNumber / 10000 % 10 + ticketNumber / 1000 % 100 % 10; // вычисляем сумму первых 3-х цифр.
+            int secondTicketPart = ticketNumber / 100 % 1000 % 100 % 10 + ticketNumber / 10 % 10000 % 1000 % 100 % 10 + ticketNumber % 100000 % 10000 % 1000 % 100 % 10; // сумма последних 3-цифр.
+            
+            if (firstTicketPart == secondTicketPart)
+                Console.WriteLine("Поздравляем! Билет №{0} является счастливым!", ticketNumber);
             else
                 Console.WriteLine("К нашему сожалению, билет №{0} не является счастливым.", ticketNumber);
+            Console.ReadKey();
         }
     }
 }
