126b20018087c13f46f4bffc4d1a5770c9370eba
khazievbulat <taras2631@gmail.com>
2017-09-28 12:52:54 +0300

    upload files

---
+++ 2017/FALL2017/SEM/SEM3/Ex3.cs
@@ -0,0 +1,29 @@
+/*
+ * Автор программы: Хазиев Булат 11-708
+ * 
+ * Описание:
+ * Дан номер трамвайного билета, в котором суммы первых трёх цифр и последних трёх цифр отличаются на 1 (но не сказано, в какую сторону). 
+ * Программа вычисляет, правда ли, что предыдущий или следующий билет является счастливым.
+*/
+
+using System;
+
+namespace ConsoleApp
+{
+    class IsTicketHappines
+    {
+        public static void Main(string[] args)
+        {
+            int
+                ticketNumber = Convert.ToInt32(Console.ReadLine()),
+                firstTicketPart = ticketNumber / 100000 + ticketNumber / 10000 % 10 + ticketNumber / 1000 % 100 % 10,
+                secondTicketPartDecrement = (ticketNumber - 1) / 100 % 1000 % 100 % 10 + (ticketNumber - 1) / 10 % 10000 % 1000 % 100 % 10 + (ticketNumber - 1) % 100000 % 10000 % 1000 % 100 % 10,
+                secondTicketPartIncrement = (ticketNumber + 1) / 100 % 1000 % 100 % 10 + (ticketNumber + 1) / 10 % 10000 % 1000 % 100 % 10 + (ticketNumber + 1) % 100000 % 10000 % 1000 % 100 % 10;
+
+            Console.WriteLine("Предыдущий билет является: {0}", (firstTicketPart == secondTicketPartDecrement) ? "Счастливым" : "Не счастивым");
+            Console.WriteLine("Следующий билет является: {0}", (firstTicketPart == secondTicketPartIncrement) ? "Счастливым" : "Не счастливым");
+
+            Console.ReadKey();
+        }
+    }
+}
