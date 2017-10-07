b9cc8fbe65444c122323d7a4328e04cb562b836e
khazievbulat <taras2631@gmail.com>
2017-10-07 19:15:00 +0300

    update algorithm

--- 2017/FALL2017/PS/PS1/Ex6.cs
+++ 2017/FALL2017/PS/PS1/Ex6.cs
@@ -2,7 +2,7 @@
  * Автор программы: Хазиев Булат 11-708
  * 
  * Описание:
- *      -Нахождение всех простых чисел от 1 до 10000.
+ *      -Нахождение всех простых чисел от 1 до 10000 (MAX_VALUE).
 */
 
 using System;
@@ -15,13 +15,21 @@ namespace ConsoleApp
 
         public static void Main(string[] args)
         {
-            for(int i = 2; i < MAX_VALUE; i++)
+            int[] filteredNumbers = new int[MAX_VALUE / 2]; // Легальное уменьшение размера массива
+            int
+                i,
+                count = 0,
+                targetValue = Convert.ToInt32(Console.ReadLine()); // Считываем индекс искомого простого числа. Отсчет начинается с 0.
+
+            for(i = 2; i < MAX_VALUE; i++)
             {
                 if (i % 2 == 0 || i % 3 == 0 || i % 5 == 0 || i % 7 == 0)
                     continue;
 
-                Console.Write("{0} ", i);
+                filteredNumbers[count++] = i;
             }
+
+            Console.WriteLine(filteredNumbers[targetValue]);
             Console.ReadKey();
         }
     }
