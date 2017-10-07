0ae4ca769f3ae38a0ee78bec5cba632dd0baa796
khazievbulat <taras2631@gmail.com>
2017-10-07 18:45:35 +0300

    update algorithm

--- 2017/FALL2017/PS/PS1/Ex3.cs
+++ 2017/FALL2017/PS/PS1/Ex3.cs
@@ -1,15 +1,42 @@
+/*
+ * Автор программы: Хазиев Булат 11-708
+ * 
+ * Описание:
+ *      -Переводит число из десятичной системы исчисления в шестнадцатиричную.
+*/
+
 using System;
 
 namespace ConsoleApp
 {
-    class ConvertNumToHex
+    class Program
     {
         public static void Main(string[] args)
         {
-            int number = 3124124; // Число, которое нужно перевести.
-            string hexNumber = Convert.ToString(number, 16); // Перевод в 16-ти ричную систему исчисления.
+            int
+                inputValue = Convert.ToInt32(Console.ReadLine()), // Считывает десятичное число (int). Например: 1000
+                tempValue = 0,
+                countValues = 0;
 
-            Console.WriteLine(hexNumber);
+            int[] resultValue = new int[inputValue.ToString().Length]; // Массив для записи итогового числа.
+            string[] changedValues = new string[6] {"A", "B", "C", "D", "E", "F"};
+
+            while(inputValue > 1)
+            {
+                resultValue[countValues++] = inputValue - inputValue / 16 * 16; // Высчитываем остаток при делении.
+                inputValue /= 16;
+            }
+
+            for(int i = 1; i < resultValue.Length; i++)
+            {
+                tempValue = resultValue[resultValue.Length - 1 - i];
+
+                switch (tempValue >= 10)
+                {
+                    case true: Console.Write(changedValues[tempValue - 10]); break;
+                    case false: Console.Write(tempValue); break;
+                }
+            }
             Console.ReadKey();
         }
     }
