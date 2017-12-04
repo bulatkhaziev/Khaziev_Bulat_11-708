e5f82d0af9319715bc8a56f56f73f2b4ff3e6780
khazievbulat <taras2631@gmail.com>
2017-12-04 14:19:07 +0300

    upload tests

---
+++ 2017/FALL2017/PS/PS2/Ex2/SolverTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Solver.Tests
+{
+    [TestClass]
+    public class SolverTest
+    {
+        [TestMethod]
+        public void FactorialTest()
+        {
+            var numbers =  new[] { 1, 2, 3, 4, 5 };
+            var factorials = new[] { 1, 2, 6, 24, 120 };
+
+            for (int i = 0; i < numbers.Length; i++)
+                Assert.AreEqual(factorials[i], EquationSolver.GetFactorial(numbers[i]));
+        }
+        
+        [TestMethod]
+        public void StepsCountTest()
+        {
+            int a = 2;
+            double x = 0.1;
+            double epsilon = 0.001;
+
+            Assert.AreEqual(8, EquationSolver.GetSumAndCountOfSteps(a, x, epsilon)[0]);
+        }
+
+        [TestMethod]
+        public void RowSummaryTest()
+        {
+            int a = 2;
+            double x = 2;
+            double epsilon = 0.001;
+
+            Assert.AreEqual(4, EquationSolver.GetSumAndCountOfSteps(a, x, epsilon)[1], epsilon);
+        }
+    }
+}
